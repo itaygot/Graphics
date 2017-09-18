102f1683f052c2ccb0ba412c275448610232bdb7
Itay Gothelf <itaygot@gmail.com>
2017-09-19 02:09:45 +0300

    notimportant

--- Include/ogldev_callbacks.h
+++ Include/ogldev_callbacks.h
@@ -1,19 +1,23 @@
 /*
 
-	Copyright 2014 Etay Meiri
+	Original file by:
+		Copyright 2014 Etay Meiri
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Modified by:
+		Itay Gothelf, 2017.
 */
 
 
@@ -33,6 +37,8 @@ public:
     virtual void RenderSceneCB() {};
 
     virtual void MouseCB(OGLDEV_MOUSE Button, OGLDEV_KEY_STATE State, int x, int y) {};
+
+	virtual void TimerCB(int value) {};
 };
 
 
