e3508577592259ec293248c20e5b6c06395e7b1e
Itay Gothelf <itaygot@gmail.com>
2018-01-01 22:00:28 +0200

    panoram stuff

--- Projects/Panorama/main.cpp
+++ Projects/Panorama/main.cpp
@@ -33,12 +33,14 @@ const char * pFSFileName = "shader.fs";
 
 //#define TEXTURE_FILENAME "../../Content/Textures/test.png"
 //#define TEXTURE_FILENAME "../../Content/Textures/earth.jpg"
-//#define TEXTURE_FILENAME "../../Content/Textures/city.jpg"
-#define TEXTURE_FILENAME "../../Content/Textures/yard.jpg"
+#define TEXTURE_FILENAME "../../Content/Textures/boat.jpg"
+//#define TEXTURE_FILENAME "../../Content/Textures/yard.jpg"
 
 
 #define WINDOW_WIDTH	768
 #define WINDOW_HEIGHT	768
+
+
 
 #define SPHERE_SEGMENTS	12
 
